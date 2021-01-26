e7b2eb892f8b901644e5459f429bbbeb146b79a3
naphteine <58526116+naphteine@users.noreply.github.com>
2021-01-26 05:54:52 +0300

    Created MainApplication class and organized code

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from tkinter import *
-from tkinter import messagebox
+import tkinter as tk
+from tkinter import messagebox as tkmsgbox
 
 # Global variables
 tasks_list = []
 counter = 1
-
-# Theme
 color_bg = "white"
 color_fg = "black"
-
 projects = [("ural", "Project manager", "Python3")]
 
 def get_rows(data):
@@ -28,67 +25,77 @@ class Table:
 		self.rows = get_rows(data)
 		self.columns = get_columns(data)
 
-		self.e = Label(gui, text="Name", fg=color_fg, bg=color_bg)
+		self.e = tk.Label(self.root, text="Name", fg=color_fg, bg=color_bg)
 		self.e.grid(row=self.start_row, column=0)
-		self.e = Label(gui, text="Description", fg=color_fg, bg=color_bg)
+		self.e = tk.Label(self.root, text="Description", fg=color_fg, bg=color_bg)
 		self.e.grid(row=self.start_row, column=1)
-		self.e = Label(gui, text="Language", fg=color_fg, bg=color_bg)
+		self.e = tk.Label(self.root, text="Language", fg=color_fg, bg=color_bg)
 		self.e.grid(row=self.start_row, column=2)
 
 		for i in range(self.rows):
 			for j in range(self.columns):
-				self.e = Entry(self.root, width=20, fg=color_fg, bg=color_bg)
+				self.e = tk.Entry(self.root, width=20, fg=color_fg, bg=color_bg)
 				self.e.grid(row=1 + self.start_row + i, column=j)
-				self.e.insert(END, self.data[i][j])
+				self.e.insert(tk.END, self.data[i][j])
 
 	def get_final_row():
 		return start_row + get_rows(self.rows)
 
-def clear():
-	entryPName.delete(0, END)
-	entryPDesc.delete(0, END)
-	entryPLang.delete(0, END)
+class MainApplication(tk.Frame):
+	def __init__(self, master):
+		self.master = master
+		tk.Frame.__init__(self, self.master)
+		self.configure_gui()
+		self.create_widgets()
 
-def submit():
-	global counter
+	def configure_gui(self):
+		self.master.configure(background="white")
+		self.master.title("ural")
+		self.master.geometry("800x800")
+		self.master.minsize(width=800, height=800)
 
-	if entryPName.get() == "" or entryPDesc.get() == "" or entryPLang.get() == "":
-		messagebox.showerror("Input error!")
-		return
+	def create_widgets(self):
+		labelPName = tk.Label(self.master, text="Project name:", fg=color_fg, bg=color_bg)
+		labelPDesc = tk.Label(self.master, text="Description:", fg=color_fg, bg=color_bg)
+		labelPLang = tk.Label(self.master, text="Language:", fg=color_fg, bg=color_bg)
+		self.entryPName = tk.Entry(self.master, bg=color_bg)
+		self.entryPDesc = tk.Entry(self.master, bg=color_bg)
+		self.entryPLang = tk.Entry(self.master, bg=color_bg)
 
-	data = [entryPName.get(), entryPDesc.get(), entryPLang.get()]
-	projects.append(data)
-	table.update_table(projects)
-	clear()
+		labelPName.grid(row=0, column=0)
+		labelPDesc.grid(row=1, column=0)
+		labelPLang.grid(row=2, column=0)
+		self.entryPName.grid(row=0, column=1)
+		self.entryPDesc.grid(row=1, column=1)
+		self.entryPLang.grid(row=2, column=1)
 
-if __name__ == "__main__":
-	gui = Tk()
-	gui.configure(background="white")
-	gui.title("ural")
-	gui.geometry("800x800")
-	gui.minsize(width=800, height=800)
+		buttonClear = tk.Button(self.master, text="Clear", fg=color_fg, bg=color_bg, command=self.clear)
+		buttonSubmit = tk.Button(self.master, text="Submit", fg=color_fg, bg=color_bg, command=self.submit)
+		buttonExit = tk.Button(self.master, text="Exit", fg=color_fg, bg=color_bg, command=exit)
+		buttonClear.grid(row=3, column=0)
+		buttonSubmit.grid(row=3, column=1)
+		buttonExit.grid(row=3, column=2)
 
-	labelPName = Label(gui, text="Project name: ", fg=color_fg, bg=color_bg)
-	labelPDesc = Label(gui, text="Description: ", fg=color_fg, bg=color_bg)
-	labelPLang = Label(gui, text="Language: ", fg=color_fg, bg=color_bg)
-	entryPName = Entry(gui, bg=color_bg)
-	entryPDesc = Entry(gui, bg=color_bg)
-	entryPLang = Entry(gui, bg=color_bg)
+		self.table = Table(self.master, 4, projects)
 
-	labelPName.grid(row=0, column=0)
-	labelPDesc.grid(row=1, column=0)
-	labelPLang.grid(row=2, column=0)
-	entryPName.grid(row=0, column=1)
-	entryPDesc.grid(row=1, column=1)
-	entryPLang.grid(row=2, column=1)
+	def submit(self):
+		global counter
 
-	buttonClear = Button(gui, text="Clear", fg=color_fg, bg=color_bg, command=clear)
-	buttonSubmit = Button(gui, text="Submit", fg=color_fg, bg=color_bg, command=submit)
-	buttonExit = Button(gui, text="Exit", fg=color_fg, bg=color_bg, command=exit)
-	buttonClear.grid(row=3, column=0)
-	buttonSubmit.grid(row=3, column=1)
-	buttonExit.grid(row=3, column=2)
+		if self.entryPName.get() == "" or self.entryPDesc.get() == "" or self.entryPLang.get() == "":
+			tkmsgbox.showerror("Input error!")
+			return
 
-	table = Table(gui, 4, projects)
+		data = [self.entryPName.get(), self.entryPDesc.get(), self.entryPLang.get()]
+		projects.append(data)
+		self.table.update_table(projects)
+		self.clear()
 
-	gui.mainloop()
+	def clear(self):
+		self.entryPName.delete(0, tk.END)
+		self.entryPDesc.delete(0, tk.END)
+		self.entryPLang.delete(0, tk.END)
+
+if __name__ == '__main__':
+	root = tk.Tk()
+	main_app = MainApplication(root)
+	root.mainloop()
